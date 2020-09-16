535f772d9d106b9f5742de730b20387de8586a2a
Noah Ye <38360170+yefangzhou1997@users.noreply.github.com>
2020-09-15 21:11:57 -0400

    Update [88]Merge Sorted Array.java

--- [88]Merge Sorted Array.java
+++ [88]Merge Sorted Array.java
@@ -26,3 +26,23 @@ public void merge(int[] nums1, int m, int[] nums2, int n) {
             }//nums1 j m
         }//nums2 i n
     }
+
+public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int k = m + n - 1, i = m - 1, j = n - 1; k >= 0; k--) {
+            if (i >= 0 && j >= 0) {
+                if (nums1[i] >= nums2[j]) {
+                    nums1[k] = nums1[i];
+                    i--;
+                } else {
+                    nums1[k] = nums2[j];
+                    j--;
+                }
+            } else if (i >= 0 && j < 0) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+        }
+    }
