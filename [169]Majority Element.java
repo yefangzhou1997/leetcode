4772e2719ec3829b6447e75d2e5bfa6dd84ef81d
Noah Ye <38360170+yefangzhou1997@users.noreply.github.com>
2020-09-23 19:50:02 -0400

    Update [169]Majority Element.java

--- [169]Majority Element.java
+++ [169]Majority Element.java
@@ -18,3 +18,37 @@ public int majorityElement(int[] nums) {
         }
         return nums[maxIndex];
     }
+
+public int majorityElement(int[] nums) {
+
+        int n = nums.length / 2;
+
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] == nums[j]) {
+                    temp[j]++;
+
+                    if (temp[j] >= n) {
+                        return nums[j];
+                    }
+
+                    break;
+                }
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        int maxIndex = -1;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (temp[i] >= n) {
+                return nums[i];
+            }
+
+            if (temp[i] > max) {
+                max = temp[i];
+                maxIndex = i;
+            }
+        }
+        return nums[maxIndex];
+    }
