3e85325fb92c990581c08d124c35b7b8ee44bf8e
Noah Ye <38360170+yefangzhou1997@users.noreply.github.com>
2020-10-01 21:38:26 -0400

    Update [204]Count Primes.java

--- [204]Count Primes.java
+++ [204]Count Primes.java
@@ -9,3 +9,16 @@ public int countPrimes(int n) {
         for (int i = 2; i * i <= n; i++) if (n % i == 0) return false;
         return true;
     }
+
+public int countPrimes(int n) {
+        int r = 0;
+        boolean[] a = new boolean[n + 1];
+
+        for (int i = 2; i < n; i++) {
+            if (!a[i]) {
+                for (int j = i + i; j < n; j += i) a[j] = true;
+                r++;
+            }
+        }
+        return r;
+    }
