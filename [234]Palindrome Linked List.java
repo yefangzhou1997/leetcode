cbc8f62dc62806645e460e0d89262bcbd645df25
Noah Ye <38360170+yefangzhou1997@users.noreply.github.com>
2020-10-04 23:14:42 -0400

    Update [234]Palindrome Linked List.java

--- [234]Palindrome Linked List.java
+++ [234]Palindrome Linked List.java
@@ -13,3 +13,34 @@ public boolean isPalindrome(ListNode head) {
         }
         return true;
     }
+
+
+public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        ListNode f = head;
+        ListNode s = head;
+
+        while (f.next != null && f.next.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+
+        ListNode reverseHead = reverseList(s.next);
+
+        while (head != null && reverseHead != null) {
+            if (head.val != reverseHead.val) return false;
+            head = head.next;
+            reverseHead = reverseHead.next;
+        }
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode theRight = head.next;
+        ListNode reverseList = reverseList(theRight);
+        theRight.next = head;
+        head.next = null;
+        return reverseList;
+    }
