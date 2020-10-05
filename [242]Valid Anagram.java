c1d20ce825a0ece537d27a32a0461ee6d8787477
Noah Ye <38360170+yefangzhou1997@users.noreply.github.com>
2020-10-05 17:22:37 -0400

    Update [242]Valid Anagram.java

--- [242]Valid Anagram.java
+++ [242]Valid Anagram.java
@@ -1,24 +1,14 @@
-public boolean isAnagram(String s, String t) {
+ public boolean isAnagram(String s, String t) {
+        int len = s.length();
+        if (len != t.length()) return false;
 
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
+        char[] x = s.toCharArray();
+        char[] y = t.toCharArray();
 
-        if (a.length != b.length) return false;
-
-        int[] x = new int[a.length];
-        int[] y = new int[b.length];
-
-        for (int i = 0; i < a.length; i++) {
-            x[i] = (int) a[i];
-            y[i] = (int) b[i];
-        }
         Arrays.sort(x);
         Arrays.sort(y);
 
-        for (int i = 0; i < x.length; i++) {
-            if (x[i] != y[i]) return false;
-        }
+        for (int i = 0; i < len; i++) if (x[i] != y[i]) return false;
 
         return true;
-        
     }
