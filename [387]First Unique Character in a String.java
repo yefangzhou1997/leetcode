01d324af8c75d6d37272adc13490f8c82aaf7a7d
Noah Ye <38360170+yefangzhou1997@users.noreply.github.com>
2020-10-13 00:11:21 -0400

    Update [387]First Unique Character in a String.java

--- [387]First Unique Character in a String.java
+++ [387]First Unique Character in a String.java
@@ -25,3 +25,10 @@ public int firstUniqChar(String s) {
         return -1;
 
     }
+
+public int firstUniqChar(String s) {
+        int[] c = new int[26];
+        for (int i = 0; i < s.length(); i++) c[s.charAt(i) - 'a']++;
+        for (int i = 0; i < s.length(); i++) if (c[s.charAt(i) - 'a'] == 1) return i;
+        return -1;
+    }
