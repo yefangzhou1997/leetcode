53c3342743e25549cd8e6d453b78fd244bb24b65
Noah Ye <38360170+yefangzhou1997@users.noreply.github.com>
2020-10-15 19:37:54 -0400

    Update [8]String to Integer (atoi).java

--- [8]String to Integer (atoi).java
+++ [8]String to Integer (atoi).java
@@ -1,67 +1,39 @@
 public int myAtoi(String s) {
+        if (s.equals("")) return 0;
 
-        if (s.equals("") || s == null) return 0;
-
-        int number = 0;
-        int letter = 0;
-        int sign = 0;
-        int spaceAfterNumberSign = 0;
-
+        int number = 0, letter = 0, sign = 0, spaceAfterNumberSign = 0;
         String r = new String();
 
         for (int i = 0; i < s.length(); i++) {
-
             char t = s.charAt(i);
 
             if (isLetter(t)) {
-                if (number == 0) {
-                    return 0;
-                }
-
+                if (number == 0) return 0;
+                if (number == 1) break;
                 letter = 1;
-
-                if(number == 1) {
-                    break;
-                }
             }
 
-            if (isSpace(t)) {
-                if (number == 1 || sign == 1) {
-                    spaceAfterNumberSign = 1;
-                }
-            }
+            if (isSpace(t)) if (number == 1 || sign == 1) spaceAfterNumberSign = 1;
 
             if (isSign(t)) {
                 if (number == 0 && sign == 0) {
                     r += t;
                     sign = 1;
-                } else if (number == 0 && sign == 1) {
-                    return 0;
-                } else if (number == 1) {
-                    break;
                 }
+                else if (number == 0 && sign == 1) return 0;
+                else if (number == 1) break;
             }
 
             if (isNumber(t)) {
                 if (letter == 0 && spaceAfterNumberSign == 0) {
                     number = 1;
-                    r = r + t;
-                } else {
-                    break;
-                }
+                    r += t;
+                } else break;
             }
+
         }//for
-
         if (number == 0) return 0;
-
-        if (Double.valueOf(r) > Integer.MAX_VALUE) {
-            return Integer.MAX_VALUE;
-        } else if (Double.valueOf(r) < Integer.MIN_VALUE) {
-            return Integer.MIN_VALUE;
-        } else {
-            return Integer.valueOf(r);
-        }
-
+        return returnR(r);
     }
 
     private boolean isNumber(char c) {
@@ -82,4 +54,10 @@ public int myAtoi(String s) {
     private boolean isLetter(char c) {
         if (c > 57 || (c < 48 && c != 45 && c != 43 && c != 32)) return true;
         return false;
+    }
+
+    private int returnR(String r) {
+        if (Double.valueOf(r) > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        else if (Double.valueOf(r) < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        else return Integer.valueOf(r);
     }
