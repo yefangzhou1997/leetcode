5d93287004d7e66ecab0c18cea73aad61595356c
Noah Ye <38360170+yefangzhou1997@users.noreply.github.com>
2020-10-21 22:17:37 -0400

    Create [15]3Sum.java

---
+++ [15]3Sum.java
@@ -0,0 +1,18 @@
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> r = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+            while (lo < hi) {
+                if (nums[lo] + nums[hi] == sum) {
+                    r.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                    lo++; hi--;
+                } else if (nums[lo] + nums[hi] < sum) lo++;
+                else hi--;
+            }
+        }
+        return r;
+    }
