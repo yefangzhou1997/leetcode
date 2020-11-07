cdd769bb67502de130fb261f52b62497240a2f5b
Noah Ye <38360170+yefangzhou1997@users.noreply.github.com>
2020-11-07 17:58:48 -0500

    Create [55]Jump Game.java

---
+++ [55]Jump Game.java
@@ -0,0 +1,8 @@
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (max < i) return false;
+            max = (nums[i] + i > max) ? nums[i] + i : max;
+        }
+        return true;
+    }
